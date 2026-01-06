4e4fd821afec4464e1c7351f6e9ef9c8dc341569
hemantkumar1991 <hemantkumar.sahu@greychaindesign.com>
2026-01-06 13:30:51 +0530

    code save

--- src/shared/images.ts
+++ src/shared/images.ts
@@ -1,4 +1,7 @@
 export const IMAGES = {
-    logo: "/assets/icons/logo.svg",
-    notificationIcon: "/assets/icons/notification-icon.svg",
-}
+  logo: "/assets/icons/logo.svg",
+  notificationIcon: "/assets/icons/notification-icon.svg",
+  commentIcon: "/assets/icons/comment.svg",
+  exportIcon: "/assets/icons/export.svg",
+  avatarImage: "/assets/images/avatar.jpg",
+};
