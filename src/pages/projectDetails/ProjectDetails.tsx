5d8aefcf004e9d9ec2c12bc846f46eb6b00de4f9
Vipin Sahu <vipin@greychaindesign.com>
2026-01-08 14:44:51 +0530

    Feat: added project details page

---
+++ src/pages/projectDetails/ProjectDetails.tsx
@@ -0,0 +1,11 @@
+import "./ProjectDetails.scss";
+
+const ProjectDetails = () => {
+  return (
+    <div className="project-details-page-container">
+      <div>Project Details</div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
